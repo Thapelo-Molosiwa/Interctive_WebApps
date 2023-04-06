1518a4ab92611471c09928fe9e590f14c02c9c8f
Thapelo-Molosiwa <0728921928p@gmail.com>
2023-04-06 13:24:50 +0200

    Completed IWA8

--- interactive-web-apps/Projects/IWA7/Challenge 2/script.js
+++ interactive-web-apps/Projects/IWA7/Challenge 2/script.js
@@ -1,19 +1,22 @@
 const nickname = "Timmy";
 const firstname = "Timothy";
 
-// if (nickname) {
-//   console.log(`Good Morning, ${nickname}!`);
-// } else if (firstname) {
-//   console.log(`Good Morning, ${firstname}!`);
-// } else {
-//   console.log("Good Morning!");
-// }
-
-
-
-if((nickname) || (firstname)){
-
-    console.log((`Good Morning, ${nickname}`) || (`Good Morning, ${firstname}`))
-}else{
-    console.log('Good Morning')
+if (nickname) {
+  console.log(`Good Morning, ${nickname}!`);
+} else if (firstname) {
+  console.log(`Good Morning, ${firstname}!`);
+} else {
+  console.log("Good Morning!");
 }
+
+
+
+// if((nickname) || (firstname)){
+
+//     console.log((`Good Morning, ${nickname}`) || (`Good Morning, ${firstname}`))
+// }
+// // }else{
+// //     console.log('Good Morning')
+// // }
+
+
