008d0a9f72c73ef4e1fe24df595bccfb8dac0aa8
Thapelo-Molosiwa <0728921928p@gmail.com>
2023-04-18 10:21:35 +0200

    done with IWA13

--- interactive-web-apps/Projects/IWA13/script.js
+++ interactive-web-apps/Projects/IWA13/script.js
@@ -5,23 +5,64 @@ let calculated = '1'
 // Only allowed to change below
 
 
-const logCalc = () => { 
-    const isString = typeof calculated  'numerical-string'; 
+// const = logCalc () => { 
+//     const isString = typeof calculated = 'numerical-string' 
+//     const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
+//     calculated === calculatedAsNumber + 1 
+// }
+
+function logCalc() { 
+    // const isString = typeof calculated === 'string' && !isNaN(parseFloat(calculated))
+    // const calculatedAsNumber = isString ? parseFloat(calculated) : calculated
+    // return calculated === calculatedAsNumber + 1 
+
+
+    const isString = typeof calculated = 'numerical-string' 
     const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
     calculated === calculatedAsNumber + 1 
+  }
+
+
+// const  calcUser () => {
+//   logCalc
+//   if (calculated > 2) user = 'John'
+//   if (calculated > 2) state = 'requesting'
+//   if (calculated > 3) state = 'idle'
+// }
+
+function calcUser() {
+
+    if(calculated < 2){
+        user = 'John';
+
+        state = 'requesting';
+
+    }
+    else if(calculated < 3){
+
+        state = 'idle';
+    }
+
+   
 }
 
-const  calcUser = () => {
-  if (calculated > 2) user = 'John'
-  if (calculated > 2) state = 'requesting';
-  if (calculated > 3) state = 'idle'
+
+
+
+// const = checkUser () => {
+// 	if (user && state === 'requesting') {
+// 		console.log(`User: ${user} (${calculated})`)
+// 	}
+// }
+
+function checkUser() {
+    if(user && state === 'requesting' ){
+
+        console.log(`User: ${user} (${calculated})`) 
+    }
 }
 
-const checkUser = () => {
-	if (user && state === 'requesting') {
-		console.log(`User: ${user} (${calculated})`)
-	}
-}
+
 
 // Only allowed to change code above
 
